9f1672a6c2427f3653137d4f6c4efa9457890e00
Murad Murtuzaliev <35427168+muradmurtuzaliev@users.noreply.github.com>
2020-04-08 19:55:04 +0300

    refactoring

--- random_location.py
+++ random_location.py
@@ -1,34 +1,42 @@
 import random
 import webbrowser
 
-def random_location():
+
+def create_location_url():
     x = format(random.uniform(-90.0, 90.0), '.7f') 
     y = format(random.uniform(-180.0, 180.0), '.7f')
+    url = "https://www.google.ru/maps/@" + x + "," + y + ",12z"
+    return url
 
-    xy = "https://www.google.ru/maps/@" + x + "," + y + ",12z"
-
-    return webbrowser.open(xy)
 
 def validator(command):
     while True:
-        if command == "generate":
-            random_location()
+        if command.lower() == "generate":
+            url = create_location_url()
+            webbrowser.open(url)
             print("\n")
             break            
         else:
             command = input("Команда введена неверно, повторите попытку: ")
 
-command = input("Чтобы сгенерирорвать случайные координаты\n"
-       "и открыть их на сайте Google Maps, введите \"generate\":\n\n")
 
-validator(command)
+def main():
+    command = input("Чтобы сгенерирорвать случайные координаты\n"
+        "и открыть их на сайте Google Maps, введите \"generate\":\n")
+    validator(command)
+    while True:
+        question = input("Введите нужную Вам команду:\n"
+            "restart -  сгенерировать новые координаты\n"
+            "quit - завершить работу\n")
+        if question.lower() == "restart":
+            print("\n")
+            url = create_location_url()
+            webbrowser.open(url)
+        elif question.lower() == "quit":
+            break       
+        else:
+            print("\n")     
 
-while True:
-    question = input("Введите нужную Вам команду:\n\n"
-          "restart -  сгенерировать новые координаты\n"
-          "quit - завершить работу\n\n")
-    if question == "restart":
-        print("\n")
-        random_location()
-    elif question == "quit":
-        break            
+
+if __name__ == "__main__":
+    main()
